e9b0090b276e6723c78f3a93fad0115e6392d916
Sampo Jaakkola <sampo.jaakkola@myy.haaga-helia.fi>
2024-01-23 22:52:23 +0200

    Calculator app to Git

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [result, setResult] = useState("");
+
+  const addition = () => {
+    setResult(Number(number1) + Number(number2));
+  };
+
+  const substraction = () => {
+    setResult(number1 - number2);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.fields}>
+        <Text>Result: {result}</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(number) => setNumber1(number)}
+          value={number1}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={(number) => setNumber2(number)}
+          value={number2}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.buttons}>
+        <Button title="+" onPress={addition} />
+        <Button title="-" onPress={substraction} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +50,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  fields: {
+    alignItems: "center",
+  },
+  input: {
+    width: 200,
+    borderColor: "grey",
+    borderWidth: 1,
+  },
+  buttons: {
+    paddingTop: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
   },
 });
